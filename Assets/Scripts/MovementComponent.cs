fba6bd099488f96e88c7f9f75fc7374ec5adaf92
lazynorie <chengjy1991@gmail.com>
2022-01-26 14:57:09 -0500

    Week3 - Lab Submission
    added animations for:
    running, reloading
    added yaw and pitch

--- Assets/Scripts/MovementComponent.cs
+++ Assets/Scripts/MovementComponent.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Numerics;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Quaternion = UnityEngine.Quaternion;
 using Vector2 = UnityEngine.Vector2;
 using Vector3 = UnityEngine.Vector3;
 
@@ -18,15 +19,21 @@ public class MovementComponent : MonoBehaviour
     private PlayerController playerController;
     private Rigidbody rigidbody;
     private Animator playeranimator;
+    public GameObject followTransform;
 
     //movement references
     private Vector2 inputVector = Vector2.zero;
     private Vector3 moveDirection = Vector3.zero;
-
+    private Vector2 lookInput = Vector2.zero;
+    
+    public float aimSensitivity = 1.0f;
+    
     public readonly int movementXHash = Animator.StringToHash("MovementX");
     public readonly int movementYHash = Animator.StringToHash("MovementY");
     public readonly int isJumpingHash = Animator.StringToHash("isJumping");
     public readonly int isRunningHash = Animator.StringToHash("isRunning");
+    public readonly int isFiringHash = Animator.StringToHash("isFiring");
+    public readonly int isReloadHash = Animator.StringToHash("isReloading");
 
     
     public void Awake()
@@ -45,7 +52,32 @@ public class MovementComponent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if (playerController.isJumping) return;
+        //x-axis ratation
+        followTransform.transform.rotation *= Quaternion.AngleAxis(lookInput.x * aimSensitivity, Vector3.up);
+        //
+        followTransform.transform.rotation *= Quaternion.AngleAxis(lookInput.y * aimSensitivity, Vector3.left);
+
+        var angles = followTransform.transform.localEulerAngles;
+        angles.z = 0;
+
+        var angle = followTransform.transform.localEulerAngles.x;
+
+        if (angle>180 && angle<300)
+        {
+            angles.x = 300;
+        }
+        else if (angle<180 && angle>70)
+        {
+            angles.x = 70;
+        }
+
+        followTransform.transform.localEulerAngles = angles;
+        
+        //rotate the play along with aim
+        transform.rotation = Quaternion.Euler(0, followTransform.transform.rotation.eulerAngles.y, 0);
+        followTransform.transform.localEulerAngles = new Vector3(angles.x, 0, 0);
+
+        if (playerController.isJumping) return;
         if (!(inputVector.magnitude > 0)) moveDirection = Vector3.zero;
 
         moveDirection = transform.forward * inputVector.y + transform.right * inputVector.x;
@@ -84,6 +116,31 @@ public class MovementComponent : MonoBehaviour
         rigidbody.AddForce((transform.up + moveDirection)*jumpForce,ForceMode.Impulse);
         
         playeranimator.SetBool(isJumpingHash, playerController.isJumping);
+    }
+
+    public void OnAim(InputValue value)
+    {
+        playerController.isAiming = value.isPressed;
+    }
+    
+    public void OnLook(InputValue value)
+    {
+        lookInput = value.Get<Vector2>();
+        //if we aim up down adjust aniations to have a mask taht will let us properly animate aim
+    }
+    
+    public void OnFire(InputValue value)
+    {
+        playerController.isFiring = value.isPressed;
+        playeranimator.SetBool(isFiringHash,playerController.isFiring);
+        //set up firing animation
+    }
+    
+    public void OnReload(InputValue value)
+    {
+        playerController.isReloading = value.isPressed;
+        playeranimator.SetBool(isReloadHash,playerController.isReloading);
+
     }
 
     private void OnCollisionEnter(Collision other)
