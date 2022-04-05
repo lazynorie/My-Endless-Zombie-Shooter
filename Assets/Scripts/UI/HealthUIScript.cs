139f480991d24085ccd0214ee09867b3f7d3ee71
lazynorie <chengjy1991@gmail.com>
2022-04-05 15:30:14 -0400

    added:
    playerhealth UI

---
+++ Assets/Scripts/UI/HealthUIScript.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HealthUIScript : MonoBehaviour
+{
+    public TextMeshProUGUI healthText;
+    public TextMeshProUGUI maxHealthText;
+    private HealthComponent playerHealthComponent;
+
+    private void OnHealthInitialized(HealthComponent healthComponent)
+    {
+        playerHealthComponent = healthComponent;
+    }
+    private void OnEnable()
+    {
+        PlayerEvents.OnHealthInitialized += OnHealthInitialized;
+    }
+
+    private void OnDisable()
+    {
+        PlayerEvents.OnHealthInitialized -= OnHealthInitialized;
+    }
+
+    private void Update()
+    {
+        healthText.text = playerHealthComponent.CurrentHealth.ToString();
+        maxHealthText.text = playerHealthComponent.MaxHealth.ToString();
+    }
+}
