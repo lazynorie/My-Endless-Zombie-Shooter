483ad4c717ba11b281ff0eb6810d60ddd60854fc
lazynorie <chengjy1991@gmail.com>
2022-04-05 13:21:58 -0400

    Week 11 Lab

---
+++ Assets/Scripts/ManagerScript/HealthComponent.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthComponent : MonoBehaviour, IDamagable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(float damage)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Destroy()
+    {
+        throw new System.NotImplementedException();
+    }
+}
