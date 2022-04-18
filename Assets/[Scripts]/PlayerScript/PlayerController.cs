45d5f199069bd4a6ae7cebc782e67842ed36a251
lazynorie <chengjy1991@gmail.com>
2022-04-17 22:29:34 -0400

    added:
    item pick up functions

--- Assets/[Scripts]/PlayerScript/PlayerController.cs
+++ Assets/[Scripts]/PlayerScript/PlayerController.cs
@@ -1,7 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 { 
     public bool isFiring;
@@ -10,5 +11,39 @@ public class PlayerController : MonoBehaviour
     public bool isRunning;
     public bool isAiming;
 
+    public bool inInventory;
     public InventoryComponent inventory;
+    public GameUIController uiController;
+
+    public void Awake()
+    {
+        inventory = GetComponent<InventoryComponent>();
+        uiController = FindObjectOfType<GameUIController>();
+    }
+
+    public void OnInventory(InputValue value)
+    {
+        if (inInventory)
+        {
+            inInventory = false;
+        }
+        else
+        {
+            inInventory = true;
+        }
+
+        OpenInventory(inInventory);
+    }
+
+    private void OpenInventory(bool open)
+    {
+        if (open)
+        {
+            uiController.EnableInventoryMenu();
+        }
+        else
+        {
+            uiController.EnableGameMenu();
+        }
+    }
 }
